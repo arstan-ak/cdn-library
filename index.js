270e2b9c4a52cb45b6b6c4a45babb0186da6b791
Arstan Akatov <arstan@mancho.dev>
2026-01-26 15:17:55 +0600

    Prepared Finik Signer for broweser side

--- index.js
+++ index.js
@@ -1,144 +1,144 @@
-"use strict";
-var __assign = (this && this.__assign) || function () {
-    __assign = Object.assign || function(t) {
-        for (var s, i = 1, n = arguments.length; i < n; i++) {
-            s = arguments[i];
-            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
-                t[p] = s[p];
+export class FinikWebClient {
+    constructor(requestData) {
+        console.log('request data:', requestData);
+        this.requestData = requestData;
+    }
+    getHttpMethod() {
+        return this.requestData.httpMethod.toLowerCase();
+    }
+    getPath() {
+        return decodeURI(this.requestData.path ?? '');
+    }
+    getHeadersData() {
+        const { headers } = this.requestData;
+        const Host = headers.Host || headers.host;
+        if (!Host) {
+            throw new Error(`Header 'Host' is required`);
         }
-        return t;
-    };
-    return __assign.apply(this, arguments);
-};
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g = Object.create((typeof Iterator === "function" ? Iterator : Object).prototype);
-    return g.next = verb(0), g["throw"] = verb(1), g["return"] = verb(2), typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (g && (g = 0, op[0] && (_ = 0)), _) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
+        const headerData = `host:${Host.toString()}`;
+        const sortedHeadersKeys = Object.keys(headers)
+            .filter((key) => key.toLowerCase().startsWith('x-api-'))
+            .sort();
+        const headersData = sortedHeadersKeys.map((key) => {
+            const value = headers[key];
+            if (typeof value === 'undefined' || value === null || typeof value.toString !== 'function') {
+                throw new Error(`Header '${key}' contains invalid value`);
             }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+            const headerData = key.toLowerCase() + ':' + value.toString();
+            return headerData;
+        });
+        return [headerData, ...headersData].join('&');
     }
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Finik = void 0;
-/* External dependencies */
-var authorizer_1 = require("@mancho.devs/authorizer");
-var uuid_1 = require("uuid");
-var Finik = /** @class */ (function () {
-    function Finik() {
+    getQueryStringParamsData() {
+        const queryParams = this.requestData.queryStringParameters ?? {};
+        const sortedQueryParamKeys = Object.keys(queryParams).sort();
+        const queryParamsData = sortedQueryParamKeys.map((key) => {
+            const value = queryParams[key] ?? '';
+            const queryParamData = encodeURI(decodeURI(key)) + '=' + encodeURI(decodeURI(value));
+            return queryParamData;
+        });
+        return queryParamsData.join('&');
     }
-    Finik.prototype.getBaseUrl = function () {
-        var baseUrl = undefined === "prod"
-            ? "https://api.acquiring.averspay.kg"
-            : "https://beta.api.acquiring.averspay.kg";
-        return baseUrl;
-    };
-    Finik.prototype.getHostFromUrl = function (url) {
-        var host = new URL(url).host;
-        return host;
-    };
-    Finik.prototype.prepareHeaders = function (apiKey) {
-        var baseUrl = this.getBaseUrl();
-        var host = this.getHostFromUrl(baseUrl);
-        var timestamp = Date.now().toString();
-        var headers = {
-            Host: host,
-            "x-api-key": apiKey,
-            "x-api-timestamp": timestamp,
-        };
-        if (!this.headers) {
-            this.headers = headers;
+    getJsonBody() {
+        let body = '';
+        if (this.requestData.body) {
+            // sort object by key
+            const sortedBody = Object.entries(this.requestData.body)
+                .sort((a, b) => a[0].localeCompare(b[0]))
+                .reduce((result, [key, value]) => {
+                result[key] = value;
+                return result;
+            }, {});
+            body = JSON.stringify(sortedBody);
         }
-        return this.headers;
-    };
-    Finik.prototype.prepareRequestData = function (input) {
-        var apiKey = input.apiKey, body = input.body;
-        var paymentId = (0, uuid_1.v4)();
-        body["PaymentId"] = paymentId;
-        var requestData = {
-            httpMethod: "POST",
-            path: "/v1/payment",
-            headers: this.prepareHeaders(apiKey),
-            queryStringParameters: undefined,
-            body: body,
-        };
-        return requestData;
-    };
-    Finik.prototype.generateSignature = function (requestData, privateKey) {
-        return __awaiter(this, void 0, void 0, function () {
-            var signature;
-            return __generator(this, function (_a) {
-                switch (_a.label) {
-                    case 0: return [4 /*yield*/, new authorizer_1.Signer(requestData).sign(privateKey)];
-                    case 1:
-                        signature = _a.sent();
-                        return [2 /*return*/, signature];
-                }
-            });
-        });
-    };
-    Finik.prototype.makePayment = function (input) {
-        return __awaiter(this, void 0, void 0, function () {
-            var apiKey, privateKey, body, requestData, signature, url, res, paymentUrl, _a, _b, _c;
-            return __generator(this, function (_d) {
-                switch (_d.label) {
-                    case 0:
-                        apiKey = input.apiKey, privateKey = input.privateKey, body = input.body;
-                        requestData = this.prepareRequestData(input);
-                        return [4 /*yield*/, this.generateSignature(requestData, privateKey)];
-                    case 1:
-                        signature = _d.sent();
-                        url = "".concat(this.getBaseUrl()).concat(requestData.path);
-                        return [4 /*yield*/, fetch(url, {
-                                method: requestData.httpMethod,
-                                headers: __assign(__assign({}, this.prepareHeaders(apiKey)), { "content-type": "application/json", signature: signature }),
-                                body: JSON.stringify(body),
-                                redirect: "manual",
-                            })];
-                    case 2:
-                        res = _d.sent();
-                        if (!(res.status === 302)) return [3 /*break*/, 3];
-                        paymentUrl = res.headers.get("location");
-                        console.log("paymentURL", paymentUrl);
-                        return [2 /*return*/, paymentUrl];
-                    case 3:
-                        _b = (_a = console).error;
-                        _c = [res.status];
-                        return [4 /*yield*/, res.text()];
-                    case 4:
-                        _b.apply(_a, _c.concat([_d.sent()]));
-                        return [2 /*return*/, "error caused"];
-                }
-            });
-        });
-    };
-    return Finik;
-}());
-exports.Finik = Finik;
+        return body;
+    }
+    getData() {
+        const parts = [];
+        const queryString = this.getQueryStringParamsData();
+        parts.push(this.getHttpMethod());
+        parts.push(this.getPath());
+        parts.push(this.getHeadersData());
+        if (queryString) {
+            parts.push(queryString);
+        }
+        parts.push(this.getJsonBody());
+        return parts.join('\n');
+    }
+    /**
+     * Import RSA private key from PEM format
+     */
+    async importPrivateKey(pemKey) {
+        // Remove PEM headers and whitespace
+        const pemContents = pemKey
+            .replace('-----BEGIN PRIVATE KEY-----', '')
+            .replace('-----END PRIVATE KEY-----', '')
+            .replace('-----BEGIN RSA PRIVATE KEY-----', '')
+            .replace('-----END RSA PRIVATE KEY-----', '')
+            .replace(/\s/g, '');
+        // Decode base64 to binary
+        const binaryKey = Uint8Array.from(atob(pemContents), (c) => c.charCodeAt(0));
+        // Import key into Web Crypto API
+        const privateKey = await window.crypto.subtle.importKey('pkcs8', binaryKey, {
+            name: 'RSASSA-PKCS1-v1_5',
+            hash: 'SHA-256',
+        }, false, // not extractable for security
+        ['sign']);
+        return privateKey;
+    }
+    /**
+     * Sign the request data using RSA-SHA256
+     */
+    async sign(privateKeyPem) {
+        const privateKey = await this.importPrivateKey(privateKeyPem);
+        const data = this.getData();
+        console.log('Data to be signed:', data);
+        // Convert string to bytes
+        const encoder = new TextEncoder();
+        const dataBytes = encoder.encode(data);
+        // Sign the data
+        const signatureBuffer = await window.crypto.subtle.sign('RSASSA-PKCS1-v1_5', privateKey, dataBytes);
+        // Convert signature to base64
+        const signatureArray = Array.from(new Uint8Array(signatureBuffer));
+        const signatureBase64 = btoa(String.fromCharCode(...signatureArray));
+        return signatureBase64;
+    }
+    /**
+     * Import RSA public key from PEM format
+     */
+    async importPublicKey(pemKey) {
+        // Remove PEM headers and whitespace
+        const pemContents = pemKey
+            .replace('-----BEGIN PUBLIC KEY-----', '')
+            .replace('-----END PUBLIC KEY-----', '')
+            .replace('-----BEGIN RSA PUBLIC KEY-----', '')
+            .replace('-----END RSA PUBLIC KEY-----', '')
+            .replace(/\s/g, '');
+        // Decode base64 to binary
+        const binaryKey = Uint8Array.from(atob(pemContents), (c) => c.charCodeAt(0));
+        // Import key into Web Crypto API
+        const publicKey = await window.crypto.subtle.importKey('spki', binaryKey, {
+            name: 'RSASSA-PKCS1-v1_5',
+            hash: 'SHA-256',
+        }, false, ['verify']);
+        return publicKey;
+    }
+    /**
+     * Verify the signature using RSA-SHA256
+     */
+    async verify(publicKeyPem, signatureBase64) {
+        const publicKey = await this.importPublicKey(publicKeyPem);
+        const data = this.getData();
+        console.log('Data to be verified:', data);
+        // Convert string to bytes
+        const encoder = new TextEncoder();
+        const dataBytes = encoder.encode(data);
+        // Convert base64 signature to bytes
+        const signatureString = atob(signatureBase64);
+        const signatureBytes = Uint8Array.from(signatureString, (c) => c.charCodeAt(0));
+        // Verify the signature
+        const isValid = await window.crypto.subtle.verify('RSASSA-PKCS1-v1_5', publicKey, signatureBytes, dataBytes);
+        return isValid;
+    }
+}
+//# sourceMappingURL=index.js.map
